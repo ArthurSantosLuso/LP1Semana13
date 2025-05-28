f8d6d291a2d96520b444a1ff23eb3e515cad1449
Arthur Santos <arthurluiz.developer@gmail.com>
2025-05-28 14:19:23 +0100

    Exercicio 1 inicialozado.

---
+++ Palindrome/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Palindrome
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            
+        }
+
+        private bool IsPalindrome(string text)
+        {
+            bool Palindrome()
+            {
+                // Verificar se é palindrome
+                return true;
+            }
+            if (text == null) throw new ArgumentNullException();
+
+            if (text.Length > 0 && text.Length < 3)
+                return true;
+            else return Palindrome();
+        }
+    }
+}
