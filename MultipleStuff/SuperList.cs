8c8b58109d26a5f32e73558797c520cf7753252e
Arthur Santos <arthurluiz.developer@gmail.com>
2025-05-28 14:40:31 +0100

    GetMinMax1 implementado.

---
+++ MultipleStuff/SuperList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleStuff
+{
+    class SuperList : List<Double>
+    {
+        private void GetMinMax1(List<double> list, out double min, out double max)
+        {
+            min = list.Min();
+            max = list.Max();
+        }
+    }
+}
