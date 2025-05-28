385a5860399ddcd0a19d1261b839476ca644b0e0
Arthur Santos <arthurluiz.developer@gmail.com>
2025-05-28 15:06:35 +0100

    Exercicio 5 finalizado

--- MultipleStuff/Program.cs
+++ MultipleStuff/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
 
 namespace MultipleStuff
 {
@@ -7,10 +8,34 @@ namespace MultipleStuff
     {
         private static void Main(string[] args)
         {
-            List<double> lista = new List<double>(){ 1, 2, 3, 4, 5, 6 };
-            SuperList sprList = new SuperList();
+            SuperList sprList = new SuperList(new List<double>() { 1, 2, 3, 4, 5, 6 });
 
+            // Versão 1
 
+            double min = default;
+            double max = default;
+
+            sprList.GetMinMax1(out min, out max);
+
+            Console.WriteLine($"Versão 1: Min: {min} Max: {max}");
+
+            // Versão 2
+
+            MinMaxStruct minMax = sprList.GetMinMax2();
+
+            Console.WriteLine($"Versão 2: Min: {minMax.Min} Max: {minMax.Max}");
+
+            // Versão 3
+
+            Tuple<double, double> tupleMinMax = sprList.GetMinMax3();
+
+            Console.WriteLine($"Versão 3: Min: {tupleMinMax.Item1} Max: {tupleMinMax.Item2}");
+
+            // Versão 4
+
+            (double, double) tupleValueMinMax = sprList.GetMinMax4();
+
+            Console.WriteLine($"Versão 4: Min: {tupleValueMinMax.Item1} Max: {tupleValueMinMax.Item2}");
         }
     }
 }
